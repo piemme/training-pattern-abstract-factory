8d028369ded7de0808880841095df8b21063fdbd
piemme <manca@eurixgroup.com>
2016-04-18 22:25:10 +0200

    correzione

--- src/org/acme/piemme/biancheria/BiancheriaFactory.java
+++ src/org/acme/piemme/biancheria/BiancheriaFactory.java
@@ -2,21 +2,21 @@ package org.acme.piemme.biancheria;
 
 import org.acme.piemme.biancheria.starwars.StarwarsBiancheriaFactory;
 import org.acme.piemme.biancheria.winx.WinxBiancheriaFactory;
+
 /**
- * Partecipante: nessuno
- * utility per che restituisce la concrete factory 
- * La logica potrebbe essere contenuta nella AbstractBiancheriaFactory
- * quindi AbstractBiancheriaFactory diventerebbe classe astratta e non interfaccia
+ * Partecipante: nessuno utility per che restituisce la concrete factory La
+ * logica potrebbe essere contenuta nella AbstractBiancheriaFactory quindi
+ * AbstractBiancheriaFactory diventerebbe classe astratta e non interfaccia
  */
-public class BiancheriaFactory  {
+public class BiancheriaFactory {
 	public static AbstractBiancheriaFactory getFactoryBy(String tipoBiancheria) {
-	     switch (tipoBiancheria) {
-         case "Starwars":
-        	 return new StarwarsBiancheriaFactory();
-         case "Winx":
-        	 return new WinxBiancheriaFactory();
-         default:
-             throw new IllegalArgumentException("tipo biancheria non valida: " + tipoBiancheria);
-	     }		
+		switch (tipoBiancheria) {
+		case "Starwars":
+			return new StarwarsBiancheriaFactory();
+		case "Winx":
+			return new WinxBiancheriaFactory();
+		default:
+			throw new IllegalArgumentException("tipo biancheria non valida: " + tipoBiancheria);
+		}
 	}
 }
