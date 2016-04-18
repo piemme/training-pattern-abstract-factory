dc473f6778fa2bdb8add89d777eb6afadfd09cbe
piemme <manca@eurixgroup.com>
2016-04-18 22:24:13 +0200

    correzione

--- src/org/acme/piemme/biancheria/AbstractBiancheriaFactory.java
+++ src/org/acme/piemme/biancheria/AbstractBiancheriaFactory.java
@@ -4,7 +4,7 @@ package org.acme.piemme.biancheria;
  * Partecipante: AbstractFactory
  * dichiara le operazioni che creano i prodotti astratti
  * E' un'interfaccia, ma potrebbe essere implementata come classe astratta
- * che aggiunge la logica attualmente presente in org.acme.piemme.Assistente
+ * che aggiunge la logica attualmente presente in org.acme.piemme.biancheria.BiancheriaFactory
  */
 public interface AbstractBiancheriaFactory {
 	Lenzuolo createLenzuolo();
